cd58d1dc090f7ec7ef63646dcc932a3b7359d851
weizwz <1124725517@qq.com>
2025-11-14 17:22:42 +0800

    删除无用的.gitkeep文件

--- components/navigation/CategorySidebar.tsx
+++ components/navigation/CategorySidebar.tsx
@@ -323,7 +323,7 @@ const CategorySidebarBase: React.FC<CategorySidebarProps> = ({ className, style 
         </DndContext>
         
         {/* 添加分类按钮 */}
-        <div className="p-4 border-t border-gray-200 dark:border-neutral-700">
+        <div className="p-4 border-t text-sm border-gray-200 dark:border-neutral-700">
           <Button
             type="dashed"
             icon={<PlusOutlined />}
